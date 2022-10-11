21105b8524af3c149636f351358379f036e8ea9a
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-10-11 20:21:12 +0200

    done

---
+++ main.js
@@ -0,0 +1,58 @@
+import { clients, refreshClients } from "../../private/clients.js";
+
+export class App {
+  client;
+
+  constructor(config) {
+    this.config = config;
+    this.findClient();
+  }
+
+  async updateDefinitions() {
+    if (!this.client) this.findClient();
+    if (!this.client) return;
+    let result = await this.client.request("http", "request", [
+      "POST",
+      this.config.url,
+      JSON.stringify({
+        arguments: [this.config.pwd],
+        export: "listFlows",
+        module: "public/flow.js",
+      }),
+      "application/json",
+    ]);
+
+    for (let functionName in this.definitions.methods)
+      delete this[functionName];
+
+    if (!result.result.success) return;
+
+    let flows = JSON.parse(result.result.response).data;
+    this.updateDefinitions.methods = {};
+
+    for (let flowName of flows) {
+      this.definitions.methods[flowName] = {
+        arguments: [],
+        name: flowName,
+      };
+
+      this[flowName] = async () => {
+        return await this.client.request("http", "request", [
+          "POST",
+          this.config.url,
+          JSON.stringify({
+            arguments: [this.config.pwd, flowName],
+            export: "runFlowSync",
+            module: "public/flow.js",
+          }),
+          "application/json",
+        ]);
+      };
+    }
+  }
+
+  async findClient() {
+    await refreshClients();
+    if (clients[this.config.client]) this.client = clients[this.config.client];
+  }
+}
